0e3b1b91789a3c58bf9aee215e46aaf164bc3a1a
Ryan <132001783+TimTheBig@users.noreply.github.com>
2025-07-19 15:10:46 -0400

    Update info in init.sh

--- init.sh
+++ init.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-# create activate miniconda env
+# create and activate miniconda env
+echo "creating a python3.10 env"
 conda create --name atomizer python=3.10
 echo "activating env"
 conda activate atomizer
